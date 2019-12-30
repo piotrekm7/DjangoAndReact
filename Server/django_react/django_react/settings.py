fc32719ceb776d265deb104ecb7cbf9e0e0287bf
Piotr Muras <piotrekm7@gmail.com>
2019-12-30 19:10:58 +0100

    deploy on google cloud

--- Server/django_react/django_react/settings.py
+++ Server/django_react/django_react/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = datastore_client.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
