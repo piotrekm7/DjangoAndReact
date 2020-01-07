97b068221a8d77accb65c674ca653bf4742af6d5
Piotr Muras <piotrekm7@gmail.com>
2020-01-07 22:43:53 +0100

    Updates

---
+++ precache-manifest.ffbbdbd07d1fae3f0bb18052437fd887.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4042b9c6a8030683998d5220b324d6",
+    "url": "/DjangoAndReact/index.html"
+  },
+  {
+    "revision": "5f4e7f933a98b25e736b",
+    "url": "/DjangoAndReact/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "7b42bd9c3fa100d545d4",
+    "url": "/DjangoAndReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f4e7f933a98b25e736b",
+    "url": "/DjangoAndReact/static/js/2.2ba2d290.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/DjangoAndReact/static/js/2.2ba2d290.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b42bd9c3fa100d545d4",
+    "url": "/DjangoAndReact/static/js/main.12acad47.chunk.js"
+  },
+  {
+    "revision": "d0a0c6a439fd7045a827",
+    "url": "/DjangoAndReact/static/js/runtime-main.fb6a7a19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DjangoAndReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
